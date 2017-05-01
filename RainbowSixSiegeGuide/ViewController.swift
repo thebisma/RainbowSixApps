2967d484946dfb9b625a88b4645d889b41d18915
bisma <bisma@bismas-iMac.local>
2017-05-01 14:26:44 +0700

    Initial Commit

---
+++ RainbowSixSiegeGuide/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  RainbowSixSiegeGuide
+//
+//  Created by iosdev on 4/29/17.
+//  Copyright © 2017 iosdev. All rights reserved.
+//
+
+import UIKit
+import FBSDKShareKit
+import FBSDKLoginKit
+class ViewController: UIViewController ,FBSDKLoginButtonDelegate{
+
+@IBOutlet weak var loginButton: FBSDKLoginButton!
+    override func viewDidLoad() {
+        let loginButton = FBSDKLoginButton()
+        // Optional: Place the button in the center of your view.
+        loginButton.center = view.center
+        view.addSubview(loginButton)
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+
+ 
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if (error != nil)
+        
+        {
+            print(error.localizedDescription)
+        }
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Goodbye")
+    }
+
+}
+
